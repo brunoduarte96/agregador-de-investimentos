d37946243405e42070a4501308942524da4313cd
brunoduarte96 <brunoduarte96@outlook.com>
2024-01-08 20:27:49 -0300

    Initial commit

---
+++ src/test/java/duarte/br/agregadordeinvestimentos/AgregadordeinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package duarte.br.agregadordeinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadordeinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
